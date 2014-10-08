db7aa88694b30329226405d8d1924afb91ae9763
Reinout Stevens <resteven@vub.ac.be>
2014-10-08 15:45:11 +0200

    removed unused code

--- damp.qwalkeko.plugin/src/qwalkeko/clj/ast.clj
+++ damp.qwalkeko.plugin/src/qwalkeko/clj/ast.clj
@@ -3,44 +3,6 @@
   (:require [damp.ekeko.jdt
              [ast :as jdt]]))
 
-;;reification of Java functions
-(defn compilation-unit? [astnode]
-  (= (.getNodeType astnode) org.eclipse.jdt.core.dom.ASTNode/COMPILATION_UNIT))
-
-(defn get-ast-path [ast]
-  (let [root (.getRoot ast)]
-    (when (compilation-unit? root)
-      (let [javaelement (.getJavaElement root)] ;;should be a org.eclipse.jdt.internal.core.CompilationUnit
-        (.removeFirstSegments (.getPath javaelement) 1)))))
-
-
-(defn string-to-path [string]
-  (new org.eclipse.core.runtime.Path string))
-
-(defn path-to-string [path]
-  (.toString path))
-
-;;logicify them
-(defn path-string [?path ?string]
-  (logic/all
-    (logic/conde
-      [(logic/nonlvaro ?path)
-       (logic/project [?path]
-         (logic/== ?string (path-to-string ?path)))]
-      [(logic/nonlvaro ?string)
-       (logic/project [?string]
-         (logic/== ?path (string-to-path ?string)))])))
-
-(defn ast-path [?ast ?path]
-  (logic/all
-    (jdt/ast :ASTNode ?ast)
-    (logic/project [?ast]
-      (logic/== ?path (get-ast-path ?ast)))))
-
-
-;;As we manually parse files we can no longer rely on paths to retrieve them
-;;Instead we'll use package name + name of the class
-
 ;;modifiers
 
 (defmacro make-modifier [name]

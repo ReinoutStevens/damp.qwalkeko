6cddbaf672e24f8b4e2d7942208fe9de3ee9e479
Ward Muylaert <ward.muylaert@gmail.com>
2017-04-11 11:07:26 +0200

    Fix compilation error, add some documentation

--- damp.qwalkeko.plugin/src/qwalkeko/experiments/automated.clj
+++ damp.qwalkeko.plugin/src/qwalkeko/experiments/automated.clj
@@ -11,7 +11,8 @@
   ;(:require [qwalkeko.clj.graph-algo :as algo])
   (:require [qwalkeko.clj.changenavigation :as nav])
   (:require [damp.ekeko.jdt
-             [ast :as jdt]]))
+             [ast :as jdt]])
+  (:require [clojure.repl :as repl]))
 
 
 ;;Folder met al de projecten
@@ -21,11 +22,18 @@
 
 
 ;;importeer project
+(defn import-project-location
+  "Given a File object of a .git folder, imports the project into the Eclipse
+   workspace in which Qwalkeko is active. The imported project will have only
+   a project.xml file in it which is the meta information that Qwalkeko needs
+   to reason about it. The imported project is given the correct Eclipse nature
+   for Qwalkeko. The imported project is also given the correct Eclipse nature
+   for Ekeko such that Ekeko queries can be cast over it. Note that Qwalkeko
+   overrides the usual Ekeko building with its own (HistoryProjectModel.buildMetaProduct),
+   which creates a resources/ folder within the project.
 
-
-     
-     
-(defn import-project-location [location] ;;location is file object naar .git
+   The code here is a clojure translation of the code in ImportRepositoryHandler.java"
+  [location]
   (let [project-name (.getName (.getParentFile location))
         root (.getRoot (org.eclipse.core.resources.ResourcesPlugin/getWorkspace))
         project (.getProject root project-name)]
@@ -45,23 +53,31 @@
       ;;add sleep
       (.refreshLocal project org.eclipse.core.resources.IProject/DEPTH_INFINITE nil)
       project)))
-    
-(defn convert-project-to-graph [project]
+
+(defn convert-project-to-graph
+  "Given an eclipse project (not ekeko project), creates the graph after
+   finding the right Ekeko project."
+  [project]
   (let [projects (damp.ekeko.ekekomodel/all-project-models)
         history (first (filter #(= (.getProject %) project) projects))]
     (graph/convert-model-to-graph history)))
 
 
-(defn find-commit-in-graph [graph id]
+(defn find-commit-in-graph
+  "Given a project graph and a commit SHA, finds the commit object"
+  [graph id]
   (first (filter #(= (graph/revision-number %) id) (:versions graph))))
 
 
-(defn do-magic [graph breaking fixing]
-  (let [changed (filter #(= (:status %) :edit) (graph/file-infos fixing))
-        changes (doall
-                  (map 
+(defn find-changes
+  "Given a graph and the breaking and fixing commits, returns a list of changed
+   files and the changes. For the changes, qwalkeko is used."
+  [graph breaking fixing]
+  (let [changed (filter #(= (:status %) :edit) (graph/file-infos fixing)) ; take only edits of files of the fixing commit (not new/deleted files)
+        changes (doall ; force evaluation
+                  (map
                     (fn [changed]
-                      (l/qwalkeko 1 [?changes]
+                      (l/qwalkeko 1 [?changes ?end]
                         (qwal/qwal graph breaking ?end [?left ?right]
                           (l/in-source-code [curr]
                             (l/fileinfo|compilationunit changed ?left curr))
@@ -72,8 +88,7 @@
                             (l/fileinfo|compilationunit changed ?right curr)
                             (changes/ast-ast-changes ?left ?right ?changes)))))
                     changed))]
+    ; Qwalkeko creates folders of the versions it analyses. Clean them up.
     (graph/ensure-delete breaking)
     (graph/ensure-delete fixing)
     (list changed changes)))
-        
-              
